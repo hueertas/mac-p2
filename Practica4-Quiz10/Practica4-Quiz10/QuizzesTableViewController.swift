9fa05c58c940d8a8784c89cb178128f89828383a
hueertas <56135013+hueertas@users.noreply.github.com>
2019-11-22 20:02:09 +0100

    Update QuizzesTableViewController.swift

--- Practica4-Quiz10/Practica4-Quiz10/QuizzesTableViewController.swift
+++ Practica4-Quiz10/Practica4-Quiz10/QuizzesTableViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class QuizzesTableViewController: UITableViewController {
     
     var quiz10Model = Quiz10Model()
+  
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        quiz10Model.download()
-        tableView.reloadData()
+        quiz10Model.download() //cambias el contenido de la tabla con el download ( se lo asignas l objeto quiz10model)
+        tableView.reloadData() //tableview:variable que proporciona los viewtable controler y ejecuta la tabla.En este caso 
+        //se carga la tabla con sus filas...
 
     }
     
@@ -42,21 +44,42 @@ class QuizzesTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-
+    
+    //las preguntas del data source y la tabla se implementa y se pinta a partir de aqui
+    //hay que rellenar los metodos del data source 
+    
+    
+    
+    
+    // Esta tableview m esta preguntando cuantas seciones hay y como se van a poner todas las preguntas seguidas,
+    //solo hay una seccion (return 1)
+    
+    
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
+    
+    
+    
+    // Esta tableview m esta preguntando cuantas filas hay en la seccion --> quizzes es el array con tods las preguntas ( hay 77 preg)
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return quiz10Model.quizzes.count
     }
 
+   //este tableview me dice dame una celda para ponerlo en el idexPath -->siempre mismo patron  
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Quiz Cell", for: indexPath) as! QuizCell
-
+        
+        
+        // saco una celda que tienes que identificar siempre 
         // Configure the cell...
         
         let row = indexPath.row
